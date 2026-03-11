7b6196feaf8a47cf62efb3b32013d805cc2da3fb
Vince Tupas <admiral.tupas@gmail.com>
2026-03-11 13:40:12 +0800

    lab-act-instance

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>welcome to My React App</h1>
+        <p>
+          Name: Vince Tupas <br />
+          Email: bisnisyows@gmail.com <br />
+          Section: INF231 <br />
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App
